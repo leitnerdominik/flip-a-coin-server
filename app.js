841643b9cae0427e305f7d78cb347037f60b143c
Dominik Leitner <bigtoasti@gmail.com>
2019-06-16 20:42:08 +0200

    adjust access-control-allow-origin

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_HOST);
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader(
     'Access-Control-Allow-Methods',
